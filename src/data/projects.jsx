75b4f31500ae3da223f99bf8e1c4a4a082602b00
Kauan Rodrigues <kauanrdx145@gmail.com>
2023-01-25 20:28:01 -0300

    atualização das informações de projetos

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -3,15 +3,15 @@ const projects = [
         id: 0,
         name: 'Portifólio',
         image: 'https://f004.backblazeb2.com/file/portifolio-kauan/projects/portifolio-devkauan17.png',
-        linkDeploy: 'https://portifolio-devkauan17.netlify.app/',
-        linkRepo: 'https://github.com/devkauan17/portifolio-devkauan17',
+        linkDeploy: 'https://portifolio-kauandevfr.netlify.app/',
+        linkRepo: 'https://github.com/devkauan17/portifolio',
         maitenance: false
     },
     {
         id: 1,
         name: 'buscador de perfis do gitHub',
         image: 'https://f004.backblazeb2.com/file/portifolio-kauan/projects/github-finder-devkauan17.png',
-        linkDeploy: 'https://github-finder-devkauan17.netlify.app/',
+        linkDeploy: 'https://github-finder-kauandevfr.netlify.app/',
         linkRepo: 'https://github.com/devkauan17/github-profile-finder',
         maitenance: false
     },
@@ -19,9 +19,9 @@ const projects = [
         id: 2,
         name: 'Gerenciador de tarefas',
         image: 'https://f004.backblazeb2.com/file/portifolio-kauan/projects/tasks-management-devkauan17.png',
-        linkDeploy: 'https://tasks-management-devkauan17.netlify.app/',
+        linkDeploy: 'https://tasks-management-kauandevfr.netlify.app/',
         linkRepo: 'https://github.com/devkauan17/tasks-management',
-        maitenance: true
+        maitenance: false
     },
 ]
 
